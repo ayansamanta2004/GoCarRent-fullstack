47baf0f64b73aded1e3211f92202901f63c7c45d
ayansamanta2004 <ayansamanta802@gmail.com>
2026-07-23 09:45:14 +0530

    Update banner, sidebar, and models

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     role: { type: String, enum: ["owner", "user"], default: 'user' },
     image: { type: String, default: '' },
 }, { timestamps: true })
